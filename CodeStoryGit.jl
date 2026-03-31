5c0716679eaf523ca89ba108a484b8cf5359f253
Morgan <a12549445@unet.univie.ac.at>
2026-03-31 14:19:35 +0200

    Introducing Factorial function

--- CodeStoryGit.jl
+++ CodeStoryGit.jl
@@ -1,15 +1,23 @@
 ## I have deleted everything, starting fresh for CodeStory
 
-
-# <>step "To find a permutation we need to first consider the variables that go in, our function will be accepting n and k from the formula"
-function PermutationCalculation(n,k)
+# <>step The following function will find the factorial for you
+function myfactorial(x)
+# <>step "This will loop down through factorials"
+    factorialsum = 1
+    while x > 0
+        factorialsum = factorialsum * x
+        x -= 1
+    end
+    return factorialsum
+# step<> 
+end
 # step<>
-# <>step "We know that n and k will need factorial calculations, lets assume we've made a Factorial function already, which takes a number input"
+
+
+
+function PermutationCalculation(n,k)
     nfac = myfactorial(n)
     kfac = myfactorial(k)
-# step<>
-# <>step "Now we put it together, and return the function value
     Permutations = Int64(nfac/myfactorial((n-k)))
     return Permutations
 end
-# step<> "Next up, we will build the factorial function which allows this to work"
