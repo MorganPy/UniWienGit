8d802c1fe5dd737312277c52e52acbe7b04edf7a
Morgan <a12549445@unet.univie.ac.at>
2026-03-31 15:29:54 +0200

    Function content testing with "CodeStory"

---
+++ TestCases.jl
@@ -0,0 +1,22 @@
+
+
+
+#<step explain="This step should include all functions without function code"
+function TestFunction()
+    #<step explain="This step should include function code 1"
+    x = 1
+    #</step>
+end
+
+function TestFunction2()
+    #<step explain="This step should include function code 2"
+    x = 2
+    #</step>
+end
+
+function TestFunction3()
+    #<step explain="This step should include function code 3"
+    x = 3
+    #</step>
+end
+#</step>
